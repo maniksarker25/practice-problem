383f84b68b2f61267e30e2d5251d6b1f02750666
manik <maniksarker652@gmail.com>
2024-11-14 23:31:52 +0600

    solve alphebet problem

---
+++ count_me_four.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+char s[10001];
+
+int main(){
+    scanf("%s",s);
+    int freq[26] = {0};
+
+    int s_len = strlen(s);
+
+    for(int i = 0; i < s_len;i++){
+        char ch = s[i];
+
+    int index = ch - 'a';
+    freq[index]++;
+    }
+
+    for(int i = 0;i<26;i++){
+        if(freq[i] > 0){
+            printf("%c - %d\n",i +'a',freq[i]);
+        }
+    }
+
+    return 0;
+}
