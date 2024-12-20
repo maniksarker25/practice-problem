ab50be9203edb7d231d28c3880666c525a086e5a
manik <maniksarker652@gmail.com>
2024-12-20 18:54:13 +0600

    solve digit sum

--- sum_digit.cpp
+++ sum_digit.cpp
@@ -4,9 +4,20 @@ using namespace std;
 
 int main()
 {
-  int n;
-  cin >> n;
-  int a[n];
-  for()
+    int n;
+    cin >> n;
+    char a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + a[i] - '0';
+    }
+
+    cout << sum;
     return 0;
 }
